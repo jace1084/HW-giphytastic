57a4e887475d67137050b5540e5d09c9fae13be5
jace <jace.sf84@gmail.com>
2018-07-06 16:20:33 -0700

    added the ajax and some for loops.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -35,7 +35,35 @@ $(document).ready(function(){
 		});
 	}
 
-	//Now, create a function to remove the last button. 
-	
+	//Now, create a function to remove the last button.
+	function removeButton(){
+		var char = $(this).attr("data-name");
+		var queryURL = 
+
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		})
+
+		// This will show the results of the gifs
+		.done(function(res){
+			$("#gifsView").empty();
+			var results = res.data;
+			if(res == ""){
+				alert("Aww, I don't have a Giphy for that one, try again!!");
+
+			}
+			for (var i = 0; i<res.length; i++);
+				//This will put the gifs in the specified div
+				var gifDiv = $("<div>");
+				//This is going to add the rating of the gif
+				var gifRating = $("<p>").text("Rating " + res[i].rating);
+				gifDiv.append(gifRating);
+
+		//This will now pull the Gif from the site
+
+		})
+	} 
+
 	
 })
