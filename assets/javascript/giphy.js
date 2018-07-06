14fc883322bd635895b0b9df16a160debea661a8
jace <jace.sf84@gmail.com>
2018-07-06 15:40:08 -0700

    added new function newButtons

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,22 +2,40 @@ $(document).ready(function(){
 
 	//first, create my original array that will (hopefully) be updated with the users choices.
 
-	var char = ["Princess Leia", "Luke Skywalker", "Anakin Skywalker", "Yoda", "R2-D2", "C-3PO", "Han Solo", "Chubacca"];
+	var subj = ["Princess Leia", "Luke Skywalker", "Anakin Skywalker", "Yoda", "R2-D2", "C-3PO", "Han Solo", "Chubacca"];
 
 	//Now, create a function that will display the gif buttons...loop throught the array
 
 	function disButtons(){
 		$("#buttonsView").empty();
-		for(var i=0; i<char.length; i++){
+		for(var i=0; i<subj.length; i++){
 			var gifButton = $("<button>");
 			var gifButton.addClass("fav");
 			var gifButton.addClass("btn btn-primary");
-			gifButton.attr("data-name", char[i]);
-			gifButton.text(char[i]);
+			gifButton.attr("data-name", subj[i]);
+			gifButton.text(subj[i]);
 			$("#buttonsView").append(gifButton);
 		}
 	}
 
 	//Next, create a function that will display the gif buttons
+
+	function newButton(){
+		$("#addGif").on("click", function(){
+			var char = $("#subjInput").val().trim();
+			if (char == ""){
+				return false; //This will stop blank buttons from being created
+			}
+
+			subj.push(char);
+
+			disButtons();
+			return false;
+
+		});
+	}
+
+	//Now, create a function to remove the last button. 
+	
 	
 })
