f454b06f1d47a879e83954ee9b1ef36ffdae2eed
jace <jace.sf84@gmail.com>
2018-07-05 17:58:49 -0700

    added the js file and started the main functions

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	//first, create my original array that will (hopefully) be updated with the users choices.
+
+	var char = ["Princess Leia", "Luke Skywalker", "Anakin Skywalker", "Yoda", "R2-D2", "C-3PO", "Han Solo", "Chubacca"];
+
+	//Now, create a function that will display the gif buttons...loop throught the array
+
+	
+})
